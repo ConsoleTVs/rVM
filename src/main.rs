66997923a1bc323cfbb316430858b8d075f8fe6d
Erik Campobadal <soc@erik.cat>
2019-10-16 04:14:53 +0200

    Updated

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ fn main() {
     let mut memory  = [0; MEMORY_SIZE];
     // Program code to execute.
     let code = [
-        Loadi(0, 20), // r0 = 20;
+        Loadi(0, 20000), // r0 = 20000;
         Loadi(1, 0), // r1 = 0;
         Compare(2, 0, 1), // r2 = r0 == r1;
         Branch(2, 2), // if (r2 == 0) goto +2;
@@ -39,7 +39,7 @@ fn main() {
     // The VM itself.
     loop {
         let op = &code[pc];
-        println!("({}) Kind = {:?}\n\tr0={}\tr1={}\tr2={}", pc, op, memory[0], memory[1], memory[2]);
+        // println!("({}) Kind = {:?}\n\tr0={}\tr1={}\tr2={}", pc, op, memory[0], memory[1], memory[2]);
         match op {
             Loadi(rx, l1) => memory[*rx as usize] = *l1,
             Addi(rx, ra, l1) => memory[*rx as usize] = memory[*ra as usize] + l1,
@@ -50,5 +50,4 @@ fn main() {
         }
         pc += 1;
     }
-    println!("Finished!");
 }
